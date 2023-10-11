e011f182a27522bb2e6789a8cded37c921472716
N0wickk <memetitv01@gmail.com>
2023-10-11 21:07:09 +0800

    add font and assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Navbar } from '../components/Navbar'
 
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <div>
-      <p>home</p>
+        opo, eto po ang /home
     </div>
+    </>
   )
 }
