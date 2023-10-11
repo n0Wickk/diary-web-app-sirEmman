94f5d5662059f9c171b6ccd9d645e56b238d1390
N0wickk <memetitv01@gmail.com>
2023-10-11 21:19:24 +0800

    added custom colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import menuIcon from  '../../assets/menu.svg'
 
 export default function Navbar() {
   return (
-    <div>
-      navbar check
-    </div>
+    <nav className='flex justify-between p-4 text-grey-400'>
+        <button>
+            <img src={menuIcon} alt="" />
+        </button>
+        <span>Today - 11 Oct</span>
+        <span></span>
+    </nav>
+
+    
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-400': '#7A70DD',
+        'green-400': '#D1E99F',
+        'white-400': '#FFFFFF',
+        'black-400': '#1E1D1E',
+        'grey-400': '#8E91A0',
+        'grey-300': '#C8CAD8',
+        'grey-200': '#F0F0F0',
+        'yellow-400': '#FFE9B1',
+      },
+    },
   },
   plugins: [],
 }
